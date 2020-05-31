6a0a145038a1ca62db04b65b7c04eefa51b2bfd2
Jaron Trossaert <jaron.trossaert@student.ucll.be>
2020-05-31 16:51:37 +0200

    some dummy data and added index to nav bar

--- src/main/java/be/ucll/ip/tasks/TasksApplication.java
+++ src/main/java/be/ucll/ip/tasks/TasksApplication.java
@@ -1,13 +1,31 @@
 package be.ucll.ip.tasks;
 
+import be.ucll.ip.tasks.model.entity.Task;
+import be.ucll.ip.tasks.model.repo.TaskRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class TasksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(TaskRepository taskRepository) {
+		return (args) -> {
+			// save a dummy task
+			Task task1 = new Task();
+			task1.setTitle("First exercise");
+			task1.setDescription("Clear the tutorial");
+			task1.setDueDate(LocalDateTime.of(2020, 10, 10, 10, 10));
+			taskRepository.save(task1);
+		};
 	}
 
 }
